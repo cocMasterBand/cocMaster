dff37217a3eaace91df7605af9ed6513cbbbb08e
LuoDi-Nate <543667537@qq.com>
2015-05-22 22:23:05 +0800

    first commit!

---
+++ firstBlood-biz/firstBlood-register/src/main/java/com/cocmaster/firstBlood_register/App.java
@@ -0,0 +1,13 @@
+package com.cocmaster.firstBlood_register;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
