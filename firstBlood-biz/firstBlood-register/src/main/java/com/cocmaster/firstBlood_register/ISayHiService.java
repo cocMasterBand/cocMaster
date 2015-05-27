8bf8804be8663051eb797f289eef5ea06470d292
LuoDi-Nate <543667537@qq.com>
2015-05-27 19:47:24 +0800

    add a helloworld

---
+++ firstBlood-biz/firstBlood-register/src/main/java/com/cocmaster/firstBlood_register/ISayHiService.java
@@ -0,0 +1,20 @@
+package com.cocmaster.firstBlood_register;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author di
+ * just for say hi
+ * @param String
+ * @return String
+ * */
+
+@Service
+public interface ISayHiService {
+	
+	/**
+	 * for test
+	 * 
+	 * */
+	public String sayHi(String name);
+}
