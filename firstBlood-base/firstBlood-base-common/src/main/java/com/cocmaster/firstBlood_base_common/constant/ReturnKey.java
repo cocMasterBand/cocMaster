559646a219ee5fb27886f81fbe725295d2456c26
LuoDi-Nate <543667537@qq.com>
2015-06-23 19:17:27 +0800

    register api

--- firstBlood-base/firstBlood-base-common/src/main/java/com/cocmaster/firstBlood_base_common/constant/ReturnKey.java
+++ firstBlood-base/firstBlood-base-common/src/main/java/com/cocmaster/firstBlood_base_common/constant/ReturnKey.java
@@ -6,9 +6,14 @@ package com.cocmaster.firstBlood_base_common.constant;
  * 需要直接在里面添加*/
 public class ReturnKey {
 	//访问成功
+	//访问成功
 	public static final int RETURN_STATUS_SUCCESS = 20000;
 	
-	
+	//接口访问相关
 	//需要登陆才能访问
-	public static final int RETURN_STATUS_NEED_LOGIN = 20010;
+	public static final int RETURN_STATUS_NEED_LOGIN = 30001;
+	
+	//注册相关
+	//手机号验证不正确
+	public static final int RETURN_STATUS_PHONECHECK_ERROR = 40001;
 }
---
+++ firstBlood-biz/firstBlood-register/src/main/java/com/cocmaster/firstBlood_register/service/IRegisterService.java
@@ -0,0 +1,19 @@
+package com.cocmaster.firstBlood_register.service;
+
+/**
+ * @author di
+ * 注册服务
+ * */
+public interface IRegisterService {
+	/**
+	 * 发送验证码给用户
+	 * 然后将
+	 * */
+	public int sendPhoneCheckKey();
+	
+	/**
+	 * 用户注册
+	 * @return int 用户id
+	 * */
+	public int userResgister();
+}
