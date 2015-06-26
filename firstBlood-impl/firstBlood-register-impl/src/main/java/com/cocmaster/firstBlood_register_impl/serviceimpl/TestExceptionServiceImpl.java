2ee8eba9d188e4d7bee84ae2166b581f1553b26a
LuoDi-Nate <543667537@qq.com>
2015-06-27 01:43:34 +0800

    tset

--- firstBlood-impl/firstBlood-register-impl/src/main/java/com/cocmaster/firstBlood_register_impl/serviceimpl/TestExceptionServiceImpl.java
+++ firstBlood-impl/firstBlood-register-impl/src/main/java/com/cocmaster/firstBlood_register_impl/serviceimpl/TestExceptionServiceImpl.java
@@ -2,11 +2,13 @@ package com.cocmaster.firstBlood_register_impl.serviceimpl;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 
 import com.cocmaster.firstBlood_base_common.exception.FirstbloodExceptionCode;
 import com.cocmaster.firstBlood_base_common.exception.ServiceException;
 import com.cocmaster.firstBlood_register.service.ITestExceptionService;
 
+@Service
 public class TestExceptionServiceImpl implements ITestExceptionService {
 	private final static Logger logger = LoggerFactory.getLogger(TestExceptionServiceImpl.class);
 
